8394b728e07b2a3f961e4dd94829a007992704f7
ChoiJiChan <pnlkc@naver.com>
2023-10-19 20:51:05 +0900

    add library

--- app/src/main/java/com/waff1e/waffle/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/waff1e/waffle/ui/login/LoginViewModel.kt
@@ -1,10 +1,13 @@
 package com.waff1e.waffle.ui.login
 
 import androidx.lifecycle.ViewModel
+import com.waff1e.waffle.data.WaffleRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class LoginViewModel @Inject constructor() : ViewModel() {
+class LoginViewModel @Inject constructor(
+    private val waffleRepository: WaffleRepository
+) : ViewModel() {
 
 }
