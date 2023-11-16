d18aefe9f96c618a933cda1a1bcb0dca0a2d44ab
Jichan Choi <pnlkc@naver.com>
2023-11-16 15:22:18 +0900

    와플 리스트 중첩 스크롤 개선

--- app/src/main/java/com/waff1e/waffle/utils/ModifierUtil.kt
+++ app/src/main/java/com/waff1e/waffle/utils/ModifierUtil.kt
@@ -6,6 +6,10 @@ import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.pullrefresh.PullRefreshState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -16,6 +20,7 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.TileMode
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.unit.IntSize
 
@@ -52,3 +57,12 @@ fun Modifier.loadingEffect(): Modifier = composed {
             size = it.size
         }
 }
+
+@OptIn(ExperimentalMaterialApi::class, ExperimentalMaterial3Api::class)
+fun Modifier.optionalNestedScroll(scrollBehavior: TopAppBarScrollBehavior, pullRefreshState: PullRefreshState) = then(
+    if (pullRefreshState.progress == 0f) {
+        Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
+    } else {
+        Modifier
+    }
+)
